10b98aab9c3fd8be1ef8d05b55c7f59563a4245d
ibiyedawo tamunomiebaka <ibiyedawotamunomiebaka@gmail.com>
2023-09-07 13:09:13 -0700

    string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main- a function that prints the string
+ * Return: program ends with 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
