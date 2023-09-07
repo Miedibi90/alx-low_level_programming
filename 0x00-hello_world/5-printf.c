fbd64fad4f0d358b23d97f3163f49f19c7f45df5
ibiyedawo tamunomiebaka <ibiyedawotamunomiebaka@gmail.com>
2023-09-07 13:37:51 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints the exact string
+ * main - prints the exact string hello, printf
  *Return: returns 0
  */
-int main(0)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
