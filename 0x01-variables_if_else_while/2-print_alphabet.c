b1415defd2f9287ec9bc4b6f59e2fd3e05b91747
ibiyedawo tamunomiebaka <ibiyedawotamunomiebaka@gmail.com>
2023-09-08 11:33:59 -0700

    small alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code; prints lowercase alphabets
+ *
+ * Return: Always 0 (Successful)
+ */
+int  main(void)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{	putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
